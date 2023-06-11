513aca9de9b1b432fb5822f0261cd525614b2ea6
kangilbin <kib3388@naver.com>
2023-06-11 15:27:21 +0900

    웹서버

---
+++ 내장 웹서버/webserver/src/test/java/com/example/webserver/WebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
