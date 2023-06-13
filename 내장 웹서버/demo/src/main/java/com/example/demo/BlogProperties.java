3227b299cd9e01192126ec442917401179d26582
kangilbin <kib3388@naver.com>
2023-06-14 00:09:51 +0900

    프로퍼티 설정

---
+++ 내장 웹서버/demo/src/main/java/com/example/demo/BlogProperties.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+
+@Component
+@ConfigurationProperties("blog")
+@Validated
+public class BlogProperties {
+
+    @NotEmpty
+    private String name;
+
+    private int age;
+
+    private String fullName;
+
+    private String dbName;
+
+    private int dbPort;
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public int getDbPort() {
+        return dbPort;
+    }
+
+    public void setDbPort(int dbPort) {
+        this.dbPort = dbPort;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
--- 내장 웹서버/demo/src/main/java/com/example/demo/SampleRunner.java
+++ 내장 웹서버/demo/src/main/java/com/example/demo/SampleRunner.java
@@ -1,25 +1,24 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRunner implements ApplicationRunner {
 
-    @Value("${blog.name}")
-    private String name;
-
     @Autowired
-    Environment environment;
+    BlogProperties blogProperties;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("=================");
-        System.out.println(name);
+        System.out.println(blogProperties.getName());
+        System.out.println(blogProperties.getAge());
+
+        System.out.println(blogProperties.getDbName());
+        System.out.println(blogProperties.getDbPort());
         System.out.println("=================");
     }
 }
