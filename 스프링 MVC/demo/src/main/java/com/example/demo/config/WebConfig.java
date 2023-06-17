726fffc5cdbb23e4794e7057f0a47babb490e4da
kangilbin <kib3388@naver.com>
2023-06-18 00:13:25 +0900

    스프링 MVC

---
+++ 스프링 MVC/demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**") // 해당 경로의 요청이 올 때
+                .addResourceLocations("classpath:/m/") // classpath 기준으로 'm' 디렉토리 밑에서 제공
+                .setCachePeriod(20);
+    }
+}
