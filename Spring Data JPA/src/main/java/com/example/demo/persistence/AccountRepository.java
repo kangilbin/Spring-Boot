80eee863540d68c8b4e9161db5f9cc313e553367
kangilbin <kib3388@naver.com>
2023-07-03 23:41:09 +0900

    JPA 데이터 초기화

--- Spring Data JPA/src/main/java/com/example/demo/persistence/AccountRepository.java
+++ Spring Data JPA/src/main/java/com/example/demo/persistence/AccountRepository.java
@@ -9,7 +9,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByUsername(String username);
 
 
-    // NativeQuery 사용 방법
-    @Query(nativeQuery = true, value = "select * from account where username='{0}'")
-    Account nativeQuety(String username);
+//     NativeQuery 사용 방법
+//    @Query(nativeQuery = true, value = "select * from account where username='{0}'")
+//    Account nativeQuety(String username);
 }
--- Spring Data JPA/src/test/java/com/example/demo/persistence/AccountRepositoryTest.java
+++ Spring Data JPA/src/test/java/com/example/demo/persistence/AccountRepositoryTest.java
@@ -5,10 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
